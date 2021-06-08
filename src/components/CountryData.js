f9dca9a18ce1a31ab366dab305ade6aa2108913e
meriemkhadraoui <miyakhad@gmail.com>
2021-06-08 13:44:42 +0200

    calling codes

--- src/components/CountryData.js
+++ src/components/CountryData.js
@@ -4,13 +4,18 @@ const CountryData = ({ result }) => {
   const country = result.map((obj, i) => {
     const { name, alpha2Code, callingCodes, capital, languages, flag } = obj;
     return (
-      <div key={i}>
-        <h2> country : {name}</h2>
-        <h3> code : {alpha2Code} </h3>
-        <h2>capital: {capital} </h2>
-
-        <h3> calling Code : {callingCodes}</h3>
-        <img src={flag} alt={name} />
+      <div key={i} className="content">
+        <div>
+          <h2> country : {name}</h2>
+          <h3> code : {alpha2Code} </h3>
+        </div>
+        <div>
+          <h2>capital: {capital} </h2>
+        </div>
+        <div>
+          <h3> calling Code : {callingCodes}</h3>
+        </div>
+        <img className="image" src={flag} alt={name} />
       </div>
     );
   });
